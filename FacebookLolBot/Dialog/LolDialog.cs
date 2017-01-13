d3674427829513f290deafb07e3af57ac5863193
AdityaJNair <adijnair@gmail.com>
2017-01-13 20:57:45 +1300

    added app id

---
+++ FacebookLolBot/Dialog/LolDialog.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace FacebookLolBot.Dialog
+{
+    public class LolDialog : IDialog<string>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
